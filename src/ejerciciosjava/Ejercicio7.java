de4bf3f43bde08cc860621207b3dc48c7b5b4530
Agustin Frankenberger <agufe94@gmail.com>
2022-05-21 12:21:59 -0300

    Commit 7

---
+++ src/ejerciciosjava/Ejercicio7.java
@@ -0,0 +1,32 @@
+/*
+Ejercicio 7
+*/
+package ejerciciosjava;
+
+import java.util.Scanner;
+
+/*
+Agustin Frankenberger
+*/
+public class Ejercicio7 {
+
+    /*
+    Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa
+pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. Nota: investigar
+la función equals() en Java.
+    */
+    
+    public static void main(String[] args) {
+       
+         Scanner leer = new Scanner (System.in);
+        System.out.println("Ingrese una palabra: ");
+        String pal;
+        pal = leer.nextLine();
+        if (pal.equals("eureka")){
+            System.out.println("Correcto");
+        } else {
+            System.out.println("Incorrecto");
+    } 
+        
+    }  
+}
